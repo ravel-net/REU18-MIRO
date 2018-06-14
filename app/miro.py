0753e1d78a8c293da43a73029d720aec504d30fd
Tony Yang <tonydmyang@gmail.com>
2018-06-14 05:17:36 -0500

    Added script

--- app/miro.py
+++ app/miro.py
@@ -14,16 +14,16 @@ class MiroConsole(AppConsole):
         #   FROM abgp INNER JOIN igp ON abgp.re = igp.re) AS alt
         # ORDER BY c ASC;
         policy = self.get_policy_sql()
+        vw_query = """CREATE OR REPLACE VIEW bgproute AS
+            SELECT d, rn, p FROM 
+            (SELECT d, igp.re, p, rn, c 
+            FROM abgp INNER JOIN igp
+            ON abgp.re = igp.re)
+            AS alt {0}
+            ORDER BY c ASC
+            LIMIT 1;""".format(policy)
         try:
-            query = """CREATE OR REPLACE VIEW bgproute AS
-                SELECT d, rn, p FROM 
-                (SELECT d, igp.re, p, rn, c 
-                FROM abgp INNER JOIN igp
-                ON abgp.re = igp.re)
-                AS alt {0}
-                ORDER BY c ASC
-                LIMIT 1;""".format(policy)
-            self.db.cursor.execute(query)
+            self.db.cursor.execute(vw_query)
             print("Success: Route in view 'bgproute'")
         except Exception, e:
             print "Failure: Unable to retrieve route", e
@@ -35,10 +35,10 @@ class MiroConsole(AppConsole):
         Gets downstream ASes.
         Usage: downstream
         """
+        vw_query = """CREATE VIEW downstream AS
+                 SELECT d, p FROM abgp GROUP BY d, p;"""
         try:
-            query = """CREATE VIEW downstream AS
-                     SELECT d, p FROM abgp GROUP BY d, p;"""
-            self.db.cursor.execute(query)
+            self.db.cursor.execute(vw_query)
             print("Success: Downstream ASes in view 'downstream'")
         except Exception, e:
             print "Failure: Unable to retrieve downstream ASes", e
@@ -47,7 +47,7 @@ class MiroConsole(AppConsole):
     # :- MIRO(D, P)
     def do_addpolicy(self, line):
         """
-        Add policy to miro_policy. First arg is prefix and rest is path.
+        Add policy. First arg is prefix and rest is path.
         Usage: addpolicy [prefix] [path]
         """
         # Enough arguments?
@@ -57,20 +57,49 @@ class MiroConsole(AppConsole):
             return
 
         # Insert into miro_policy
+        ins_query = """INSERT INTO miro_policy
+            VALUES ('{0}','{1}')""".format(args[0], ' '.join(args[1:]))
         try:
-            query = """INSERT INTO miro_policy
-                VALUES ('{0}','{1}')""".format(args[0], ' '.join(args[1:]))
-            self.db.cursor.execute(query);
+            self.db.cursor.execute(ins_query);
             print("Success: Added policy to table 'miro_policy'")
         except Exception, e:
             print "Failure: Unable to add policy", e
             return
 
+    def do_delpolicy(self, line):
+        """
+        Delete policy. First arg is prefix and rest is path.
+        Usage: delpolicy [prefix] [path]
+        """
+        # Enough arguments?
+        args = line.split(' ')
+        if len(args) < 2:
+            print "Invalid syntax"
+            return
+
+        #try to delete policy
+        sel_query = """SELECT * FROM miro_policy WHERE 
+            d='{0}' AND p='{1}'""".format(args[0], ' '.join(args[1:]))
+
+        del_query = """DELETE FROM miro_policy WHERE 
+            d='{0}' AND p='{1}'""".format(args[0], ' '.join(args[1:]))
+        try:
+            self.db.cursor.execute(sel_query);
+            if len(self.db.cursor.fetchall()) == 0:
+                print("No policies matched.")
+                return
+            self.db.cursor.execute(del_query);
+            print("Success: Deleted policy to table 'miro_policy'")
+        except Exception, e:
+            print "Failure: Unable to delete policy", e
+            return
+
     def get_policy_sql(self):
         # Get cursor to policy table
         policies = None;
+        sel_query = "SELECT * FROM miro_policy"
         try:
-            self.db.cursor.execute("SELECT * FROM miro_policy;")
+            self.db.cursor.execute(sel_query)
             policies = self.db.cursor.fetchall()
         except Exception, e:
             print "Failure: Unable to view policy", e
@@ -89,8 +118,8 @@ class MiroConsole(AppConsole):
             first = False if first==True else False
             policy_sql = "{0}{1}NOT (d='{2}' AND p='{3}')".format(policy_sql,
                     conjunction, policy[0], policy[1])
-        return policy_sql
-        
+            return policy_sql
+
 
 shortcut = "miro"
 description = "Interdomain routing policy"
--- miro.sh
+++ miro.sh
@@ -1,5 +1,26 @@
-orch load miro
+orch load miro sample
+
+sample echo "SHOW DOWNSTREAM ASes"
 miro downstream
 p SELECT * FROM downstream;
+
+sample echo "SHOW VANILLA BGP ROUTE"
 miro route
 p SELECT * FROM bgproute;
+
+sample echo "ADD POLICY, THEN RECALCULATE ROUTE"
+miro addpolicy d AS 2
+p SELECT * FROM miro_policy;
+miro route
+p SELECT * FROM bgproute;
+miro delpolicy d AS 2
+p SELECT * FROM miro_policy;
+
+#sample echo "CASE WHERE MULTIPLE CHOICES IN ABGP"
+#miro addpolicy d AS 1
+#p SELECT * FROM miro_policy;
+#miro route
+#p SELECT * FROM bgproute;
+#miro delpolicy d AS 1
+
+miro help
