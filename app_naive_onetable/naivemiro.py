13ec2193d597f4ff83349c6a23978169b4972123
tyang27 <tonydmyang@gmail.com>
2018-06-23 09:50:46 +0800

    Changed naive one table implementation to use arrays instead

--- app_naive_onetable/naivemiro.py
+++ app_naive_onetable/naivemiro.py
@@ -2,27 +2,31 @@ import cmd
 from ravel.app import AppConsole
 
 class MiroConsole(AppConsole):
-    # :- MIRO(D, P)
-    def do_addpolicy(self, line):
-        """
-        Add policy. First arg is prefix and rest is path.
-        Usage: addpolicy [prefix] [path]
-        """
-        # Enough arguments?
-        args = line.split(' ')
-        if len(args) < 2:
-            print "Invalid syntax"
-            return
+  # :- MIRO(D, P)
+  def do_addpolicy(self, line):
+    """
+    Add policy. First arg is prefix and rest is path.
+    Usage: addpolicy [prefix] [path]
+    """
+    # Enough arguments?
+    args = line.split(' ')
+    if len(args) < 2:
+      print "Invalid syntax"
+      return
+    # Is AS path numeric?
+    if not args[1].isdigit():
+      print("AS Path is not numeric")
+      return
 
-        # Insert into miro_policy
-        ins_query = """INSERT INTO miro_policy
-            VALUES ('{0}','{1}')""".format(args[0], ' '.join(args[1:]))
-        try:
-            self.db.cursor.execute(ins_query);
-            print("Success: Added policy to table 'miro_policy'")
-        except Exception, e:
-            print "Failure: Unable to add policy", e
-            return
+    # Insert into miro_policy
+    ins_query = """INSERT INTO miro_policy
+        VALUES ('{0}',{1})""".format(args[0], ' '.join(args[1:]))
+    try:
+      self.db.cursor.execute(ins_query);
+      print("Success: Added policy to table 'miro_policy'")
+    except Exception, e:
+      print "Failure: Unable to add policy", e
+      return
 
 shortcut = "miro"
 description = "Interdomain routing policy"
--- app_naive_onetable/naivemiro.sql
+++ app_naive_onetable/naivemiro.sql
@@ -2,13 +2,14 @@
 
 -- BGP table
 DROP TABLE IF EXISTS bgp CASCADE;
-CREATE UNLOGGED TABLE bgp (prefix varchar(16), ingress varchar(16), egress varchar(16), aspath varchar(16), cost int);
+CREATE UNLOGGED TABLE bgp (prefix varchar(16), ingress varchar(16), egress varchar(16), aspath int[], cost int);
 
 -- Miro policies
 DROP TABLE IF EXISTS miro_policy CASCADE;
-CREATE UNLOGGED TABLE miro_policy(prefix varchar(16), aspath varchar(16));
+CREATE UNLOGGED TABLE miro_policy(prefix varchar(16), aspath int);
 
--- ==}{== BACKEND ==}{== --
+
+-- ==}{== VIEWS ==}{== --
 
 -- Refresh route
 CREATE OR REPLACE FUNCTION route_refresh_fun() RETURNS void AS $$
@@ -16,14 +17,13 @@ hot_potato = "SELECT MIN(cost) from bgp;"
 route = """CREATE OR REPLACE VIEW route AS
     SELECT prefix, ingress, aspath 
     FROM bgp
-    WHERE igp.cost = {0};"""
+    WHERE cost = {0};"""
 
-#min_cost = plpy.execute(hot_potato)
-#min_cost = min_cost[0]['min']
-#if min_cost is not None:
-#  plpy.execute(route.format(min_cost))
+min_cost = plpy.execute(hot_potato)
+if len(min_cost) > 0 and min_cost[0]['min'] != None:
+  min_cost = min_cost[0]['min']
+  plpy.execute(route.format(min_cost))
 $$ LANGUAGE 'plpythonu' VOLATILE SECURITY DEFINER;
-
 
 -- Refresh miro view
 CREATE OR REPLACE FUNCTION miro_refresh_fun() RETURNS void AS $$
@@ -31,9 +31,15 @@ CREATE OR REPLACE VIEW miro AS SELECT prefix, aspath FROM bgp GROUP BY prefix, a
 $$ LANGUAGE SQL;
 
 
+-- ==}{== BACKEND ==}{== --
+
 -- Miro repair mechanism
 CREATE OR REPLACE FUNCTION miro_repair_fun() RETURNS TRIGGER AS $$
+newprefix = str(TD["new"]["prefix"])
+newaspath = str(TD["new"]["aspath"])
 
+delete_bgp = "DELETE FROM bgp WHERE prefix='{0}' AND {1}=ANY(aspath);"
+plpy.execute(delete_bgp.format(newprefix, newaspath))
 plpy.execute('SELECT route_refresh_fun()')
 
 return None
@@ -48,12 +54,17 @@ EXECUTE PROCEDURE miro_repair_fun();
 
 -- ==}{== TEST VALUES ==}{== --
 
-INSERT INTO bgp VALUES
-('d','E','A','AS 1',15),('d','D','A','AS 1',8),('d','C','A','AS 1',5)
-('d','E','A','AS 2',15),('d','D','A','AS 2',8),('d','C','A','AS 2',5)
-('d','E','B','AS 2',13),('d','D','B','AS 2',6),('d','C','B','AS 2',3);
+INSERT INTO bgp VALUES ('d','E','A','{1,3}',15),('d','D','A','{1,3}',8),('d','C','A','{1,3}',5),('d','E','A','{2,4}',15),('d','D','A','{2,4}',8),('d','C','A','{2,4}',5),('d','E','B','{2,4}',13),('d','D','B','{2,4}',6),('d','C','B','{2,4}',3);
+
+
+-- ==}{== POPULATE INITIAL VIEWS ==}{== --
 
 SELECT route_refresh_fun();
 SELECT miro_refresh_fun();
 
-CREATE OR REPLACE VIEW naivemiro_violation AS (SELECT * FROM igp);
+
+-- ==}{== REMOVE ERROR MESSAGE ==}{== --
+
+DROP TABLE IF EXISTS remove_error_msg;
+CREATE UNLOGGED TABLE remove_error_msg(violation int);
+CREATE OR REPLACE VIEW naivemiro_violation AS (SELECT * from remove_error_msg);
