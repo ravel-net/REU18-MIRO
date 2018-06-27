8fbfc2b96a6822cc8a03db3d35bcf90ac6ad6779
tyang27 <tonydmyang@gmail.com>
2018-06-28 04:06:00 +0800

    Parser removes duplicates

--- as_topo/parse.py
+++ as_topo/parse.py
@@ -5,13 +5,20 @@ if len(sys.argv) < 2:
     print('Not enough arguments')
     sys.exit(0)
 
+# For determining unique edges
+lines_seen = set()
+
+# Output file pointer
+ofp = open('cycles_combined.txt','w')
 for filename in sys.argv[1:]:
     with open(filename,'r') as fp:
         for l in fp:
             l = l.strip()
-            if l.startswith('D'):
+            if l.startswith('D') and l not in lines_seen:
+                lines_seen.add(l)
                 l_arr = l.replace(',','_').split('\t')
                 for eachfrom in l_arr[1].split('_'):
                     for eachto in l_arr[2].split('_'):
-                        print(eachfrom + ' ' + eachto)
-                
+                        ofp.write(eachfrom + ' ' + eachto + '\n')
+
+ofp.close()
